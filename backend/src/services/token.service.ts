d680097cb927dc999a95b7f147ca61b2015cce80
Bi Phan <bphan944@gmail.com>
2026-03-11 16:59:54 -0700

    add validation function to safely retrieve or refresh access token

--- backend/src/services/token.service.ts
+++ backend/src/services/token.service.ts
@@ -1,5 +1,8 @@
 //build methods to read and write access token in our redis db
 import redis from "../utils/redis";
+import { getRefreshToken, updateRefreshToken } from "../services/mongo.user.services";
+import type { SpotifyToken } from "../types/spotify.types";
+import {refreshAccessToken} from "../services/spotify.user.service";
 
 //write the access token to the redis db 
 export const setAccessToken = async (spotifyId: string, accessToken: string): Promise<void> => {
@@ -9,10 +12,38 @@ export const setAccessToken = async (spotifyId: string, accessToken: string): Pr
 
 //read the token from redis
 export const getAccessToken = async (spotifyId: string): Promise<string | null> => {
-    return await redis.get(`user:${spotifyId}:accessToken`);
+    const token = (await redis.get(`user:${spotifyId}:accessToken`));
+    if (!token) {
+        return null;
+    }
+    return token;
+
 }
 
 //when the user logs out delete 
 export const deleteAccessToken = async (spotifyId: string): Promise<void> => {
     await redis.del(`user:${spotifyId}:accessToken`)
 }
+
+
+//method called grab valid accessToken
+export const getValidAccessToken = async (spotifyId: string): Promise<string> => {
+    const token = await getAccessToken(spotifyId);
+    if (token) return token;
+
+    //if the access token could not be found
+    const refreshToken = await getRefreshToken(spotifyId);
+    if (!refreshToken) throw new Error("no refresh token found for this id");
+    const responseSpotifyToken = await refreshAccessToken(refreshToken);
+    
+    if (responseSpotifyToken.refresh_token && responseSpotifyToken.refresh_token != refreshToken) {
+        await updateRefreshToken(spotifyId, responseSpotifyToken.refresh_token);
+    }
+    await setAccessToken(spotifyId, responseSpotifyToken.access_token);
+    return responseSpotifyToken.access_token;
+
+
+
+}
+
+
