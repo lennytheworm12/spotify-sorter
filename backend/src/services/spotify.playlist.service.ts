391733c048edf4367130c0c6c60be4183677f9f0
Bi Phan <bphan944@gmail.com>
2026-03-22 00:11:33 -0700

    added getting songs from playlist given playlistid and accesstokne

--- backend/src/services/spotify.playlist.service.ts
+++ backend/src/services/spotify.playlist.service.ts
@@ -4,45 +4,60 @@
 
 
 import axios from "axios";
-import type { SpotifySimplifiedPlaylist, SpotifyUserPlaylistsResponse, SpotifyLikedTrack, SpotifyLikedSongsResponse} from "../types/spotify.types";
+import type { SpotifySimplifiedPlaylist, SpotifyUserPlaylistsResponse, SpotifyLikedTrack, SpotifyLikedSongsResponse, SpotifyPlaylistItem, SpotifyPlaylistItemsResponse } from "../types/spotify.types";
 
 //request to get user playlist
 
 export const getUserPlaylists = async (accessToken: string): Promise<SpotifySimplifiedPlaylist[]> => {
     //asks spotify for user playlist via /v1/me/playlists
-        const allPlaylists: SpotifySimplifiedPlaylist[] = [];
-        let url = `https://api.spotify.com/v1/me/playlists?limit=50`
+    const allPlaylists: SpotifySimplifiedPlaylist[] = [];
+    let url = `https://api.spotify.com/v1/me/playlists?limit=50`
 
-        while (url) {
-            const response = await axios.get<SpotifyUserPlaylistsResponse>(url, {
-                headers: {Authorization: `Bearer ${accessToken}`}
-            })
-            allPlaylists.push(...response.data.items);
-            url = response.data.next ?? ''; //update to the next fetch url
-        }
-        return allPlaylists;
+    while (url) {
+        const response = await axios.get<SpotifyUserPlaylistsResponse>(url, {
+            headers: { Authorization: `Bearer ${accessToken}` }
+        })
+        allPlaylists.push(...response.data.items);
+        url = response.data.next ?? ''; //update to the next fetch url
+    }
+    return allPlaylists;
 
-        
+
 }
 
 
 
-export const getUserLikedSongs = async(accessToken: string):Promise<SpotifyLikedTrack[]> => {
+export const getUserLikedSongs = async (accessToken: string): Promise<SpotifyLikedTrack[]> => {
 
     //gets the user's liked songs through 
-    const allTracks : SpotifyLikedTrack[] = [];
+    const allTracks: SpotifyLikedTrack[] = [];
     let url = `https://api.spotify.com/v1/me/tracks?limit=50`
     while (url) {
         const response = await axios.get<SpotifyLikedSongsResponse>(url, {
-                headers: {Authorization: `Bearer ${accessToken}`}
+            headers: { Authorization: `Bearer ${accessToken}` }
         })
         allTracks.push(...response.data.items);
         url = response.data.next ?? '';
     }
-    return allTracks
-
+    return allTracks;
 
 
 }
 
+
+//method to get the tracks on a playlist
+export const getPlaylistTracks = async (accessToken: string, playlistId: string): Promise<SpotifyPlaylistItem[]> => {
+    const allTracks: SpotifyPlaylistItem[] = [];
+
+    let url = `https://api.spotify.com/v1/playlists/${playlistId}/items`;
+
+    while (url) {
+        const response = await axios.get<SpotifyPlaylistItemsResponse>(url, {
+            headers: { Athorization: `Bearer ${accessToken}` }
+        })
+        allTracks.push(...response.data.items);
+        url = response.data.next ?? '';
+    }
+    return allTracks;
+}
 
