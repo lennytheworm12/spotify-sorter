249f4b88c23374c3817e273b98f3b0dd01c50981
Bi Phan <bphan944@gmail.com>
2026-04-01 23:32:53 -0700

    add GET /playlists protected route

--- backend/src/routes/playlist.routes.ts
+++ backend/src/routes/playlist.routes.ts
@@ -1,14 +1,13 @@
 import express from "express";
-
-import {verifyUser} from "../middleware/auth.middleware";
+import { verifyUser } from "../middleware/auth.middleware";
+import { getPlaylists } from "../controllers/playlist.getPlaylists";
+import { getPlaylistTracks } from "../controllers/playlist.getPlaylistTracks";
 
 const router: express.Router = express.Router();
+
 router.use(verifyUser);
 
-
-//routing for getting playlists
-// router.get('/', getPlaylists);
-//router.get('/:id/tracks', getPlaylistTracks)
-
+router.get('/', getPlaylists);
+router.get('/:id/tracks', getPlaylistTracks);
 
 export default router;
